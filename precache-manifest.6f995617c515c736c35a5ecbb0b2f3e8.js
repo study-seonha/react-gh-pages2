aea3f845b389be9d2ad602aa8cfb875cdbfb36a9
limseonha <seonha010@gmail.com>
2022-12-16 14:44:03 +0900

    Updates

---
+++ precache-manifest.6f995617c515c736c35a5ecbb0b2f3e8.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "5325da1f60c6c703690d",
+    "url": "/react-gh-pages2/static/js/main.5325da1f.chunk.js"
+  },
+  {
+    "revision": "d314b2590995e3db1cb7",
+    "url": "/react-gh-pages2/static/js/1.d314b259.chunk.js"
+  },
+  {
+    "revision": "95bb6de89803288ef4fd",
+    "url": "/react-gh-pages2/static/js/runtime~main.95bb6de8.js"
+  },
+  {
+    "revision": "2d9574dadf8a2eedc926edcf798fc337",
+    "url": "/react-gh-pages2/index.html"
+  }
+];
